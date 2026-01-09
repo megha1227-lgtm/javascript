59d8bbab5ebbe2671736405dab9388ad61a2852f
megha sharma <meghasharma123tt@gmail.com>
2026-01-09 06:52:09 +0000

    day2 of js practice set

---
+++ Day2/practice_set2.js
@@ -0,0 +1,64 @@
+//1 functions & numbers 
+
+// function hourlyRate(dailyRate){
+//     return dailyRate*8;
+
+// }
+// console.log(hourlyRate(100));
+
+// function remainingMin(expecterdMin, actualMin){
+//     return expecterdMin -actualMin;
+// }
+// console.log(remainingMin(110 ,40));
+
+
+// function totalPay(hourlyRate, DayWOrked){
+//     return hourlyRate+ DayWOrked
+// }
+// console.log(totalPay(300,299));
+
+// function fullDayFit(budget, dailyCost){
+//     const fullday = budget / dailyCost;
+//     return Math.floor(fullday);
+// }
+// console.log(fullDayFit(500,120));
+
+
+// Booleans & logic
+// && all are true , || at least one is true , ! not
+// function lo(hasKey ,hasCode){
+//     return hasKey && hasCode
+// }
+// console.log(lo(true , false));
+
+// function admin (isAdmin , isOwner){
+//     return isAdmin || isOwner
+// }
+
+// console.log(admin(false , false));
+// function  enemy(enemyIsAwake){
+//     return !enemyIsAwake
+// }
+// console.log(enemy(true));
+
+
+//3 Roundin & Math
+
+
+
+// function fullBoxes (totalItems,boxCapacity){
+//     return Math.floor(totalItems / boxCapacity);
+// }
+// console.log(fullBoxes(10, 3));
+
+// function cost(costPerItem , numberOfIems){
+//     return Math.floor(costPerItem+numberOfIems);
+// }
+
+// console.log(cost(2.5 ,4));
+
+//4 Mini Business Logic 
+
+function rate(totalBudget ,hourlyRate){
+    const fullDays= 
+}
