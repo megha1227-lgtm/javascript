a9dd7447ea01af2f2402a3e3967c18c47af45dd1
megha sharma <meghasharma123tt@gmail.com>
2026-01-21 04:57:55 +0000

    day4 folder

---
+++ Day4/practice-set4.js
@@ -0,0 +1,44 @@
+//1
+
+// function checkUser(age) {
+//     if(age<18){
+//         return ' not allowed'
+//     }else if(age===18|| age <= 60){
+//         return 'access granted'
+//     }else if (age > 60){
+//         return `senior access`
+//     }
+    
+// }
+// console.log(checkUser(70));
+// console.log(checkUser(50));
+// console.log(checkUser(60));
+// console.log(checkUser(16));
+
+//2
+
+// function checkDelivery(cartAmount) {
+//  if(cartAmount <500){
+//     return 'Delivery charged applied '
+//  }else if(cartAmount>=500 && cartAmount<=2000){
+//     return 'Free delivery'
+//  }else if (cartAmount > 2000){
+//     return `Free delivery + gift`
+//  }
+// }
+// console.log(checkDelivery(2500));
+
+
+//3
+// function greetingByTime(hour) {
+//     if(hour<=12){
+//         return 'Good morning'
+
+//     }else if(hour>=12 && hour <= 18){
+//         return 'Good afternoon'
+//     }else{
+//         return 'Good evening'
+//     }
+    
+// }
+// console.log(greetingByTime(12));
